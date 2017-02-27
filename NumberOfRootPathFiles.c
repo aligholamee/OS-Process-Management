93ea5dda0ca326c61a71746c7a267167b2c17fe8
Ali Gholami <aligholami7596@gmail.com>
2017-02-27 09:51:20 -0800

    Add piping and child command execution logic

--- NumberOfRootPathFiles.c
+++ NumberOfRootPathFiles.c
@@ -11,15 +11,22 @@ int main()
 	const char *parentCommand = (char *)"wc -l"					 /* Command to be executed by the parent process */
 
 	int tunnel[2];												 /* Defining an array of integer to let the child process store a number and parent process to pick that number */
-	pid_t pID = fork();											 /* fork from the main process */
+	pid_t pID = fork();											 /* Fork from the main process */
+
+	if (pipe(tunnel) == -1)										 /* Pipe from the parent to the child */
+		die("pipe died.");
 
 	if(pID == -1) 												 /* Check if the fork result is valid */
 	{
-		//
+		die("fork died.");
 	}	
 	else if(pID == 0) 											 /* Check if we are in the child process */	
 	{
-
+		dup2 (tunnel[1], STDOUT_FILENO);						 /* Redirect standard output */					
+		close(tunnel[0]);
+		close(tunnel[1]);
+		execl(childCommand, path);								 /* Execute the child command */
+		die("execl died.");
 	}	
 	else														 /* When we are still in the main process */
 	{
