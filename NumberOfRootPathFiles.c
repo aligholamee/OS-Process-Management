4dec305b7eb2e906fb2108fd92492e8d52815b8b
Ali Gholami <aligholami7596@gmail.com>
2017-02-27 09:40:51 -0800

    Define die function for possible errors in piping
    Also did some code clean up for the code readability.

--- NumberOfRootPathFiles.c
+++ NumberOfRootPathFiles.c
@@ -2,27 +2,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-using namespace std;
+#define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
 
 int main()
 {
-	const char *path = (char *)"/"; /* Root path */
+	const char *path = (char *)"/"; 							 /* Root path */
+	const char *childCommand = (char *)"ls |"; 					 /* Command to be executed by the child process */
+	const char *parentCommand = (char *)"wc -l"					 /* Command to be executed by the parent process */
 
-	const char *childCommand = (char *)"ls |"; /* Command to be executed by the child process */
+	int tunnel[2];												 /* Defining an array of integer to let the child process store a number and parent process to pick that number */
+	pid_t pID = fork();											 /* fork from the main process */
 
-	const char *parentCommand = (char *)"wc -l" /* Command to be executed by the parent process */
-
-	pid_t pID = fork(); /* fork from the main process */
-
-	if(pID == -1) /* Check if the fork result is valid */
+	if(pID == -1) 												 /* Check if the fork result is valid */
 	{
 		//
 	}	
-	else if(pID == 0) /* Check if we are in the child process */	
+	else if(pID == 0) 											 /* Check if we are in the child process */	
 	{
 
 	}	
-	else /* When we are still in the main process */
+	else														 /* When we are still in the main process */
 	{
 		//
 	} 		
