6f04e273e7887d5a5265ab65b3a253064b8865ef
Ali Gholami <aligholami7596@gmail.com>
2017-02-27 02:13:04 -0500

    Separate the child command from the parent command execution for piping

--- NumberOfRootPathFiles.c
+++ NumberOfRootPathFiles.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main()
 {
+	const char *path = (char *)"/"; /* Root path */
+
+	const char *childCommand = (char *)"ls |"; /* Command to be executed by the child process */
+
+	const char *parentCommand = (char *)"wc -l" /* Command to be executed by the parent process */
+
 	pid_t pID = fork(); /* fork from the main process */
 
 	if(pID == -1) /* Check if the fork result is valid */
 	{
-
+		//
 	}	
 	else if(pID == 0) /* Check if we are in the child process */	
 	{
@@ -17,6 +24,8 @@ int main()
 	}	
 	else /* When we are still in the main process */
 	{
+		//
+	} 		
 
-	} 				
+	return 0;		
 }
