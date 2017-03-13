a81d44a710a780188b5cae3a65f119f2f9c2c566
Ali Gholami <aligholami7596@gmail.com>
2017-03-13 07:17:58 -0400

    Simplified the whole code :D

--- NUMBER_OF_FILES_IN_ROOT_DIRECTORY/Number_Of_Files_In_Root_Directory.c
+++ NUMBER_OF_FILES_IN_ROOT_DIRECTORY/Number_Of_Files_In_Root_Directory.c
@@ -1,45 +1,40 @@
-#include <stdio.h>												/* Working with I/O */
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>												/* System call */
-#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
-#define die(e) do { fprintf(stderr, "%s\n", e); exit(EXIT_FAILURE); } while (0);
-#define MAX_BUF 2024
+#define READ_END 0
+#define WRITE_END 1
 
-int main()
+int main(int argc, char* argv[]) 
 {
-	int i = 0;													 /* A simple loop counter :) */
-	int counter = 0;											 /* Counts the number of lines in the string provided in the child process */
-	int dirFileNum;												 /* Keeps the list of files in the directory */
-	int tunnel[2];												 /* Defining an array of integer to let the child process store a number and parent process to pick that number */
-	pid_t pID = fork();	
-	char buf[MAX_BUF];										     /* Fork from the main process */
+    printf("There are ");
+    pid_t pid;
+    int filedescriptor[2];
 
-	int bytes_read;
+    pipe(filedescriptor);
+    pid = fork();
 
+    if(pid==0)
+    {
+        dup2(filedescriptor[WRITE_END], STDOUT_FILENO);
+        close(filedescriptor[READ_END]);
+        execlp("ls", "ls", "/", NULL);
+    }
+    else
+    { 
+        pid=fork();
 
-	if (pipe(tunnel) == -1)										 /* Pipe from the parent to the child */
-		die("pipe died.");
+        if(pid==0)
+        {
+            
+            dup2(filedescriptor[READ_END], STDIN_FILENO);
+            close(filedescriptor[WRITE_END]);
+            execlp("wc", "wc", "-l",NULL);
+        }
+    }
 
-	if(pID == -1) 												 /* Check if the fork result is valid */
-	{
-		die("fork died.");
-	}	
-	else if(pID == 0) 											 /* Check if we are in the child process */	
-	{
-		dup2 (tunnel[1], STDOUT_FILENO);						 /* Redirect standard output */					
-		close(tunnel[1]);
-		execlp("bin/ls", "bin/ls","/",(char *)NULL);		 		 /* Execute the child command */
-		//die("execl died.");
-	}	
-	else														 /* When we are still in the main process */
-	{
-		dup2 (tunnel[0], STDIN_FILENO);
-		close(tunnel[0]);
-		execlp("wc","wc","-l",(char *)NULL);
-		//wait(NULL);												 /* Wait until the job is done by the child process */		
-	} 		
-
-	return 0;		
+    printf(" files in the root directory.\n");
+    return 0;
 }
